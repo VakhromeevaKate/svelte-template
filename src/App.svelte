5e9ce22bf47fe1341c1a73e76e29fecaeb8f1fb0
evakhromeeva <evakhromeeva@ozon.ru>
2021-01-13 08:21:42 +0300

    git commit -m "first commit"

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,17 @@
 <script>
+	import NavBar from './components/NavBar.svelte';
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<NavBar title="Evil Pony" />
+	<h1>Welcome to my {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
 	}
 
 	h1 {
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let title;
+</script>
+
+<div>
+    <h1>{title}</h1>
+</div>
+
+<style>
+    div {
+        background-color: pink;
+    }
+
+    h1 {
+        color: #330000;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        div {
+            max-width: none;
+        }
+    }
+</style>
